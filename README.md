a5d762a8662127768881ffce998cc12fbb879657
Arthur Maltson <arthur@maltson.com>
2013-07-31 21:32:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# dotfiles cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "dotfiles"
+maintainer       "Arthur Maltson"
+maintainer_email "arthur.kalm at gmail.com"
+license          "MIT"
+description      "Installs/Configures dotfiles"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
